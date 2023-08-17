e49e5eaae2eb87dd9a72d3f780be42010203dfd8
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-17 22:01:43 +0400

    add navbar and logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/raad.png';
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08111e] text-gray-300'>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
