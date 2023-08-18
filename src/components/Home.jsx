1aa31c1dde3575569916af5cdcf6445e49915be6
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-18 23:36:08 +0400

    add text in home componenet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { HiArrowNarrowRight } from  'react-icons/hi';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen pic'>
+    <div name='home' className='w-full h-screen pic'>
+      <div className='max-w-[1000px] mx-auto h-full flex flex-col justify-center pt-2'>
+        <p>My Name Is</p>
+        <h1 className='text-4xl '>Peyman Raad</h1>
+        <h2>I Am React Developer & Data Scientist</h2>
+      </div>
 
     </div>
   )
