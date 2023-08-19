3130aaaded7ba185c3f298b3e5817b17fc2dfeb2
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-19 20:15:06 +0400

    add component skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div name="about" className='w-full h-screen about text-gray-300 bg-gray-800'>
+        <div name="about" className='w-full h-screen about text-gray-300 bg-gray-800 mb-'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                     <div className='sm:text-right pb-8 pl-4'>
@@ -10,11 +10,11 @@ const About = () => {
                     </div>
                     <div></div>
                 </div>
-                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 '>
                     <div className='sm:text-right text-4xl font-bold'>
                         <p>Hi , I Am Peyman ,Lorem ipsum dolor sit amet consectetur adipisicing.</p>
                     </div>
-                    <div>
+                    <div >
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat laborum, assumenda, laudantium magni voluptates libero necessitatibus maxime quas odio, fugit voluptatum dignissimos soluta saepe iste quidem quis ratione hic asperiores?
                         </p>
