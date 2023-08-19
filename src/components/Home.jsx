a021b794d6f52efb32f9a27786fccc29c6b1020c
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-19 13:59:34 +0400

    add html and css home cpmponent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,22 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen pic '>
-      <div className='max-w-[1000px] mx-auto ml-28 h-full flex flex-col justify-center  pt-2'>
-        <p className='text-[#353639] text-[40px] font-bold'>My Name Is</p>
+      <div className='max-w-[1000px] mx-auto  h-full flex flex-col justify-center px-8'>
+        <p className='text-[#353639]  '>My Name Is</p>
         <h1 className='text-4xl sm:text-7xl font-extrabold text-[60px] text-[#0d080d]'>Peyman Raad</h1>
-        <h2 className='text-4xl sm:text-7xl mb-80 font-bold text-[#252525] text-[25px] '>I Am React Developer & Data Scientist</h2>
+        <h2 className='text-4xl sm:text-7xl  font-bold text-[#252525] '>I Am React Developer & Data Scientist</h2>
+        <p className='text-[#353639] py-4 max-w-[700px]'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quo, aspernatur in corrupti sunt deserunt rerum soluta! Sapiente, doloremque facilis.
+        </p>
+
+        <div>
+          <button className='bg-teal-600 text-white rounded-full border-2 px-6 py-3 my-2 group flex items-center hover:bg-teal-900 hover:border-teal-600 '>
+            Works
+            <span className='duration-500'>
+              <HiArrowNarrowRight className='ml-4'/>
+            </span>
+          </button>
+        </div>
       </div>
 
     </div>
