15e27ae3c72271d8bd5108dd4dcd337b5c70412d
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-20 18:54:09 +0400

    finish cotact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full min-h-screen flex justify-center items-center p-4 contact bg-emerald-900'>
+    <div name='contact' className='w-full min-h-screen flex justify-center items-center p-4 contact bg-violet-900'>
 
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
@@ -11,9 +11,9 @@ const Contact = () => {
             </div>
 
             <input className='rounded-full bg-gray-700 focus:border-blue-500 focus:outline-none focus:bg-gray-800 text-gray-300 py-4 px-4'  type="text" placeholder='John doe ..' name='name' />
-            <input className='rounded-full bg-gray-700 focus:border-blue-500 focus:outline-none focus:bg-gray-800 text-gray-300 py-4 px-4' type="email" placeholder='WW@gmail.com' name='email' />
-            <textarea name="message" placeholder='Message..' rows="10"></textarea>
-            <button>Contact Me</button>
+            <input className='rounded-full bg-gray-700 focus:border-blue-500 focus:outline-none focus:bg-gray-800 text-gray-300 py-4 px-4 my-4' type="email" placeholder='WW@gmail.com' name='email' />
+            <textarea className='resize-none rounded-3xl bg-gray-700 focus:border-blue-500 focus:outline-none focus:bg-gray-800 text-gray-300 py-4 px-4' name="message" placeholder='Message..' rows="10"></textarea>
+            <button className='text-white rounded-2xl border-2 hover:bg-teal-600 hover:border-teal-600 px-4 py-3 my-8 mx-auto flex items-center'>Contact Me</button>
         </form>
       
     </div>
